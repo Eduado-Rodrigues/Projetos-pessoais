9053230629815153c3b11bc6a2000f6e0be51658
Eduardo Rodrigues Oliveira <159492869+Eduado-Rodrigues@users.noreply.github.com>
2024-03-29 19:33:59 -0300

    Ajuste na função

--- Calculadora/estilo-e-script/script.js
+++ Calculadora/estilo-e-script/script.js
@@ -1,8 +1,8 @@
 function displaynum(n1) {
-    Calculator.text.value = Calculator.text1.value + n1;
+    Calculator.text1.value = Calculator.text1.value + n1;
 }
 
 function apagarUltimoCaractere() {
     var text = document.getElementById('caixa');
-    text.value = text.value.slice(0, 1);
+    text.value = text.value.slice(0, -1);
 }
