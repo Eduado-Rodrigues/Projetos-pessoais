e02385abfa82b6438112bc04bca1ded34bc5d586
Eduardo Rodrigues Oliveira <159492869+Eduado-Rodrigues@users.noreply.github.com>
2024-04-23 10:53:40 -0300

    Cálculo

--- calculadora-de-imc/estilos-script/script.js
+++ calculadora-de-imc/estilos-script/script.js
@@ -6,4 +6,19 @@ function calcularIMC() {
         document.getElementById("resultado").innerHTML = "Por favor, insira valores válidos para altura e peso.";
         return;
     }
+
+    var imc = peso / (altura * altura);
+    var resultado = "Seu IMC é: <span class='";
+    
+    if (imc < 18.5) {
+        resultado += "abaixo-peso";
+    } else if (imc < 25) {
+        resultado += "normal";
+    } else if (imc < 30) {
+        resultado += "acima-peso";
+    } else if (imc < 40) {
+        resultado += "obeso";
+    } else {
+        resultado += "obesidade-grave";
+    }
 }
